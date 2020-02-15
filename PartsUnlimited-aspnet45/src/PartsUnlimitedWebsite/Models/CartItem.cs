297d699aa40343d651b620804bd94756160f9a3e
wilbys <wilby@wilbysanchez.com>
2020-02-15 10:53:48 +0100

    thrid change

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
+    //my thrid change
     {
         [Key]
         public int CartItemId { get; set; }
